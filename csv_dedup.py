a502e67a94995014768373261206d9ff1794ff47
eth0up <f13ftw@gmail.com>
2018-12-16 18:10:29 -0500

    set header to 0

--- csv_dedup.py
+++ csv_dedup.py
@@ -7,6 +7,6 @@ import pandas as pd # reqs 'pip install pandas'
 # pd.concat() - returns one pd.DataFrame()
 # df.drop_duplicates(keep='last') - gets  unique values (rows) by retaining last row
 # reset_index(drop = True) - resets the index in the unique list
-df = pd.concat([pd.read_csv(currentFile, header = None, index_col = False) for currentFile in glob.iglob(r'c:\users\user\documents\*.csv')], ignore_index = True).drop_duplicates(subset = None, keep = 'last').reset_index(drop = True)
+df = pd.concat([pd.read_csv(currentFile, header = 0, index_col = False) for currentFile in glob.iglob(r'c:\users\user\documents\*.csv')], ignore_index = True).drop_duplicates(subset = None, keep = 'last').reset_index(drop = True)
 
 print("df: " + str(df))
