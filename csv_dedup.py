43e4ee934618fcfb238a801906999eb457b8a9d8
eth0up <f13ftw@gmail.com>
2018-12-16 16:16:37 -0500

    initial commit

---
+++ csv_dedup.py
@@ -0,0 +1,12 @@
+import glob
+import pandas as pd # reqs 'pip install pandas'
+
+# glob.iglob(path) - returns List[str]
+# pd.read_csv(currentFile) - returns pd.DataFrame()
+# for currentFile in glob.iglob() - returns a List[DataFrames]
+# pd.concat() - returns one pd.DataFrame()
+# df.drop_duplicates(keep='last') - gets  unique values (rows) by retaining last row
+# reset_index(drop = True) - resets the index in the unique list
+df = pd.concat([pd.read_csv(currentFile, header = None, index_col = False) for currentFile in glob.iglob(r'c:\users\user\documents\*.csv')], ignore_index = True).drop_duplicates(subset = None, keep = 'last').reset_index(drop = True)
+
+print("df: " + str(df))
